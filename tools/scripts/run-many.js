f8bbb8baafab00acb3e8f208233ae14f74bda9f7
Johan Aerens <johan.aerens@securex.be>
2022-03-11 18:26:36 +0100

    first test with fast build

--- tools/scripts/run-many.js
+++ tools/scripts/run-many.js
@@ -3,17 +3,15 @@ const execSync = require('child_process').execSync;
 const target = process.argv[2];
 const jobIndex = Number(process.argv[3]);
 const jobCount = Number(process.argv[4]);
-const isMain = process.argv[5] === 'refs/heads/main';
-const baseSha = isMain ? 'origin/main~1' : 'origin/main';
+const isMaster = process.argv[5] === 'refs/head/master';
+const baseSha = isMaster ? 'origin/master~1' : 'origin/master';
 
 const affected = execSync(
   `npx nx print-affected --base=${baseSha} --target=${target}`
 ).toString('utf-8');
-const array = JSON.parse(affected)
-  .tasks.map((t) => t.target.project)
-  .slice()
-  .sort();
-const sliceSize = Math.max(Math.floor(array.length / jobCount), 1);
+const array = JSON.parse(affected).tasks.map((t) => t.target.project);
+array.sort();
+const sliceSize = Math.floor(array.length / jobCount);
 const projects =
   jobIndex < jobCount
     ? array.slice(sliceSize * (jobIndex - 1), sliceSize * jobIndex)
@@ -21,7 +19,9 @@ const projects =
 
 if (projects.length > 0) {
   execSync(
-    `npx nx run-many --target=${target} --projects=${projects} --parallel`,
+    `npx nx run-many --target=${target} --projects=${projects.join(
+      ','
+    )} --parallel}`,
     {
       stdio: [0, 1, 2],
     }
